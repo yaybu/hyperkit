e41731e3ff5c18e5a3d3c4e8b708bbbc4dc37ca1
Doug Winter <doug.winter@isotoma.com>
2014-04-28 17:51:03 +0100

    better debug message

--- hyperkit/command.py
+++ hyperkit/command.py
@@ -30,7 +30,7 @@ def guess_hypervisor(args):
         if hypervisor is None:
             print >> sys.stderr, "Specified hypervisor is not valid"
             raise SystemExit(1)
-        logging.debug("%r hypervisor selected by user" % hypervisor)
+        logging.debug("%s hypervisor selected by user" % hypervisor.hypervisor_id)
     else:
         if VirtualBox.present:
             hypervisor = VirtualBox
@@ -186,6 +186,7 @@ def net_show(args):
 
 def main():
 
+    default_username = os.environ.get('LOGNAME', os.environ.get('USER', 'hyperkit'))
     logging.basicConfig(level=logging.INFO, format="hyperkit: %(message)s")
 
     parser = argparse.ArgumentParser()
@@ -195,11 +196,11 @@ def main():
     sub = parser.add_subparsers()
 
     create_parser = sub.add_parser("create", help="Create a new virtual machine")
-    create_parser.add_argument("name", default="hyperkit", nargs="?", help="The name of the vm to create")
+    create_parser.add_argument("name", default="myvm", nargs="?", help="The name of the vm to create")
     create_parser.add_argument("distro", default="ubuntu", nargs="?", help="The name of the distro to use")
     create_parser.add_argument("release", nargs="?", help="The release of the distro to use")
     create_parser.add_argument("arch", nargs="?", help="The architecture of the distro to use")
-    create_parser.add_argument("--username", default="hyperkit", help="The username of the initial user")
+    create_parser.add_argument("--username", default=default_username, help="The username of the initial user")
     create_parser.add_argument("--password", default=None, help="The password for the initial user")
     create_parser.add_argument("--public-key", default=None, help="A specific public key to be added to the initial user's authorized list")
     create_parser.add_argument("--key-id", default=None, help="The name of a key in your ~/.ssh folder")
