e2fed17c68e4d48b5c4f253458031d96e275e0d2
Doug Winter <doug.winter@isotoma.com>
2014-04-28 17:52:12 +0100

    add hosttype switcheroo again

--- hyperkit/hypervisor/vmrun.py
+++ hyperkit/hypervisor/vmrun.py
@@ -1,4 +1,7 @@
 
+import os
+import subprocess
+
 from . import command
 
 
@@ -6,8 +9,29 @@ class VMRun(command.Command):
 
     command_name = "vmrun"
     subcommands = {
-        "start": ["startvm", "{name}", "{type}"],
-        "stop": ["stopvm", "{name}", "hard"],
+        "start": ["start", "{name}", "{type}"],
+        "stop": ["stop", "{name}", "{type}"],
         "delete": ["deleteVM", "{name}"],
         "readVariable": ["readVariable", "{name}", "guestVar", "{variable}"],
     }
+
+    @property
+    def hosttype(self):
+        default_hosttypes = [
+            'ws',
+            'fusion',
+            'player',
+        ]
+        devnull = open(os.devnull, "w")
+        for hosttype in default_hosttypes:
+            command = [self.pathname, "-T", hosttype, "list"]
+            if subprocess.call(command, stdout=devnull, stderr=devnull) == 0:
+                return hosttype
+        raise OSError("Cannot find host type")
+
+    def compose(self, subcommand, *args, **kwargs):
+        cmd_args = [self.pathname, "-T", self.hosttype]
+        for a in self.subcommands[subcommand]:
+            cmd_args.append(a.format(**kwargs))
+        cmd_args.extend(args)
+        return cmd_args
